edba993b6d49c4751f0ef2034a8f7331678e204a
twinkle-tyagi <twinkletyagi@niet.co.in>
2022-10-25 11:36:13 +0530

    db updated

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_ADMIN_ID, process.env.DB_ADMIN_PASSWORD, {
     dialect: 'mysql',
-    host: 'localhost'
+    host: process.env.DB_HOST
 })
 
 module.exports = sequelize;
